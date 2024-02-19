668790ae9908bfe63fb3ac57646a5351f45d2482
taphuonglinh <taphuonglinh@luvina.net>
2024-02-19 13:26:02 +0700

    done

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,13 +1,16 @@
 import {useState} from "react";
 
-export default function NewTask({onAddTask, onDeleteTask}) {
-    const [enteredTask, setEnteredTask] = useState();
+export default function NewTask({onAddTask}) {
+    const [enteredTask, setEnteredTask] = useState('');
 
     function handleChange(event) {
         setEnteredTask(event.target.value);
     }
 
     function handleClick() {
+        if(enteredTask.trim() === '') {
+            return;
+        }
         onAddTask(enteredTask);
         setEnteredTask('');
 
